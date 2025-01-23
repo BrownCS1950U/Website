71311b2e2bfe01728b61f69b762e211d5cef8ab0
Feiyue Zhang <feiyue_zhang@brown.edu>
2025-01-23 12:32:41 -0500

    Update lab 1 link and dates

--- src/constants/labs.js
+++ src/constants/labs.js
@@ -1,38 +1,37 @@
 export const labs = [
   {
     topic: "Introduction",
-    date: "1/22",
-    slides: ""
-    // slides: "https://docs.google.com/document/d/1cigrsK6drWl8M2O_rTFhiCyhYoMLyjwlxzKg5OntW0c/edit?usp=drive_link"
+    date: "1/23",
+    slides: "https://docs.google.com/document/d/1cigrsK6drWl8M2O_rTFhiCyhYoMLyjwlxzKg5OntW0c/edit?usp=sharing"
   },
   {
     topic: "Lab 2",
     // topic: "Inputs",
-    date: "1/29",
+    // date: "1/29",
     slides: ""
   },
   {
     topic: "Lab 3",
     // topic: "More C++",
-    date: "2/5",
+    // date: "2/5",
     slides: ""
   },
   {
     topic: "Lab 4",
     // topic: "ECS???",
-    date: "2/26",
+    // date: "2/26",
     slides: ""
   },
   {
     topic: "Lab 5",
     // topic: "Optimize for fun",
-    date: "3/12",
+    // date: "3/12",
     slides: ""
   },
   {
     topic: "Lab 6",
     // topic: "AI",
-    date: "4/2",
+    // date: "4/2",
     slides: ""
   },
 ];
